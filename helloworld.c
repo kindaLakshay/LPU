b74862741a18e007c9aa8bc68fa658b38eca2c3f
Lakshay Choudhary <iterator.one@gmail.com>
2023-02-05 20:51:11 +0530

    hlo

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+#include<stdio.h>
+
+int main(){
+    int age;
+    printf("Please enter your age here: ");
+    scanf("%d",&age);
+    printf("your age is: %d",age);
+
+    return 0;
+}
