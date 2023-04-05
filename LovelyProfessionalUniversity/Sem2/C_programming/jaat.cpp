8ea51512651f7e56124c61b43712a4a9123ddecf
Lakshay Choudhary <iterator.one@gmail.com>
2023-04-05 09:47:30 +0530

    trash

---
+++ LovelyProfessionalUniversity/Sem2/C_programming/jaat.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lux on 04-04-2023.
+//
+
+#include "jaat.h"
---
+++ LovelyProfessionalUniversity/Sem2/C_programming/jaat.h
@@ -0,0 +1,14 @@
+//
+// Created by lux on 04-04-2023.
+//
+
+#ifndef LPU_JAAT_H
+#define LPU_JAAT_H
+
+
+class jaat{
+
+};
+
+
+#endif //LPU_JAAT_H
