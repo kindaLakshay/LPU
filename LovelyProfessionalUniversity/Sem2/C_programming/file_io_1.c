865b0a46dbc97e989ffe897cd0665fa4de5ae9d0
Lakshay Choudhary <lakshaychoudhary77712@gmail.om>
2023-04-10 10:21:06 +0530

    modified first file

--- LovelyProfessionalUniversity/Sem2/C_programming/file_io_1.c
+++ LovelyProfessionalUniversity/Sem2/C_programming/file_io_1.c
@@ -1,7 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main() {
     FILE *p;
-    p = fopen("")
+    p = fopen("test_file.md", "r");
+    if (p == NULL) {
+        printf("File doesn't exist.\n");
+    } else {
+        printf("The file exists.\n");
+        int x = fclose(p);
+        if (x == 0) {
+            printf("File closed successfully.\n");
+        } else {
+            printf("Error closing file.\n");
+        }
+    }
     return 0;
 }
