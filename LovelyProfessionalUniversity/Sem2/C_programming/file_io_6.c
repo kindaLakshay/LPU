a5bfb7ad8df118fd77a7819046abcce1d0810b6e
Lakshay Choudhary <lakshaychoudhary77712@gmail.om>
2023-04-17 10:24:52 +0530

    added new files

---
+++ LovelyProfessionalUniversity/Sem2/C_programming/file_io_6.c
@@ -0,0 +1,28 @@
+//fseek()-->SEEK_CUR and rewind()#include<stdio.h>
+
+int main() {
+  FILE * fp;
+  int n, ch, pos;
+  fp = fopen("aa2.txt", "r");
+  printf("Here is the content available in file----->\n\n");
+  while ((ch = fgetc(fp)) != EOF) {
+    printf("%c", ch);
+  }
+  printf("\n\nfp=%d\n\n", ftell(fp));
+  rewind(fp);
+  printf("\n\nBeginning fp=%d\n\n", ftell(fp));
+  printf("\nenter how many characters would u like to skip");
+  scanf("%d", & n);
+
+  fseek(fp, n, SEEK_CUR);
+  printf("\nn=%d\n", n);
+  printf("\nfp=%d\n\n", ftell(fp));
+
+  printf("\n Info after %d bytes---->\n\n\n", n);
+  while ((ch = fgetc(fp)) != EOF)
+    printf("%c", ch);
+  printf("\n\n");
+  fclose(fp);
+  getch();
+  return 0;
+}
